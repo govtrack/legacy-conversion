58b771ffdeef68a690b6433084b81df6e1152f85
Joshua Tauberer <jt@occams.info>
2013-02-16 10:39:00 -0500

    some tweaks to help check for bill XML differences

--- check_xml.py
+++ check_xml.py
@@ -1,4 +1,5 @@
-import os.path
+import os.path, re
+from glob import glob
 from lxml import etree
 from simplediff import diff
 
@@ -45,58 +46,42 @@ def is_different( diff_list ):
 	return ( ( len( diff_list ) > 1 ) or ( ( diff_list != [] ) and ( diff_list[0][0] != "=" ) ) )
 
 CONGRESS = 112
-HOUSE_RESERVED = range( 1, 21 )
-SENATE_RESERVED = range( 1, 21 )
-
-OLD_PATH = "./%(congress)d/bills/%(old_code)s%(bill)d.xml"
-NEW_PATH = "../data/%(congress)d/bills/%(new_code)s/%(new_code)s%(bill)d/data.xml"
-
-old_scraper = {}
-new_scraper = {}
+OLD_PATH = "../data/us/%(congress)d/bills/%(old_code)s%(bill)d.xml"
+NEW_PATH = "../congress/data/%(congress)d/bills/%(new_code)s/%(new_code)s%(bill)d/data.xml"
 
 for old, new in [ ( "h", "hr" ), ( "hr", "hres" ), ( "hj", "hjres" ), ( "hc", "hconres" ), ( "s", "s" ), ( "sr", "sres" ), ( "sj", "sjres" ), ( "sc", "sconres" ) ]:
-	old_scraper[new] = {}
-	new_scraper[new] = {}
-	bill = 1
+	path_params = { "congress": CONGRESS, "old_code": old, "new_code": new, "bill": 999999 }
 
-	while True:
-		path_params = { "congress": CONGRESS, "old_code": old, "new_code": new, "bill": bill }
+	# Use the list of existing old files to know what to compare.
+	for oldbillpath in glob((OLD_PATH % path_params).replace("999999", "[0-9]*")):
+		bill = int(re.search(r"(\d+)\.xml$", oldbillpath).group(1))
+		
+		#print "Checking %s%d..." % ( new, bill )
+		bill_id = "%s%d..." % ( new, bill )
 
-		old_scraper[new][bill] = {}
-		new_scraper[new][bill] = {}
-
-		if not ( os.path.exists( OLD_PATH % path_params ) and os.path.exists( NEW_PATH % path_params ) ):
-			if ( ( new == "hr" ) and ( bill in HOUSE_RESERVED ) ) or ( ( new == "sr" ) and ( bill in SENATE_RESERVED ) ):
-				print "Skipping reserved bill (%s%d)" % ( new, bill )
-
-				bill += 1
-
-				continue
-			else:
-				break
-
-		print "Checking %s%d..." % ( new, bill )
-
-		old_scraper[new][bill]["parsed_tree"] = etree.parse( OLD_PATH % path_params )
-		new_scraper[new][bill]["parsed_tree"] = etree.parse( NEW_PATH % path_params )
+		path_params['bill'] = bill
+		old_tree = etree.parse( OLD_PATH % path_params )
+		new_tree = etree.parse( NEW_PATH % path_params )
 
 		# Check that the state of the bill has not changed.
-		if old_scraper[new][bill]["parsed_tree"].find( "state" ).text != new_scraper[new][bill]["parsed_tree"].find( "state" ).text:
-			print old_scraper[new][bill]["parsed_tree"].find( "state" ).text + " => " + new_scraper[new][bill]["parsed_tree"].find( "state" ).text
+		if old_tree.find( "state" ).text != new_tree.find( "state" ).text:
+			print bill_id, old_tree.find( "state" ).text + " => " + new_tree.find( "state" ).text
 
-		for old_element in old_scraper[new][bill]["parsed_tree"].iter():
-			old_path = old_scraper[new][bill]["parsed_tree"].getpath( old_element )
+		for old_element in old_tree.iter():
+			old_path = old_tree.getpath( old_element )
+			
+			if old_path.startswith("/bill/status"): continue
 
-			new_element = new_scraper[new][bill]["parsed_tree"].xpath( old_path )
+			new_element = new_tree.xpath( old_path )
 
 			# Check for a one-to-one path relationship.
 			if len( new_element ) != 1:
 				# If the list is empty, the element is missing.
 				# Otherwise, the path matches more than one element, which means elements have been added.
 				if len( new_element ) == 0:
-					print "* Element missing:", old_path
+					print bill_id, "* Element missing:", old_path
 				else:
-					print old_path, old_element, new_element
+					print bill_id, old_path, old_element, new_element
 			else:
 				old_element_keys = sorted( old_element.keys() )
 				new_element_keys = sorted( new_element[0].keys() )
@@ -106,32 +91,42 @@ for old, new in [ ( "h", "hr" ), ( "hr", "hres" ), ( "hj", "hjres" ), ( "hc", "h
 				# Check for attribute changes.
 				for state, attributes in key_diff:
 					if state != '=':
-						print "* Element has difference in attributes:", old_path
+						print bill_id, "* Element has difference in attributes:", old_path
 
 					# Check for missing or added attributes.
 					if state == "-":
 						for attribute in attributes:
-							print "** Attribute missing:", attribute + '="' + old_element.attrib[attribute] + '"'
+							print bill_id, "** Attribute missing:", attribute + '="' + old_element.attrib[attribute] + '"'
 					elif state == "+":
 						for attribute in attributes:
-							print "** Attribute added:", attribute + '="' + new_element[0].attrib[attribute] + '"'
+							print bill_id, "** Attribute added:", attribute + '="' + new_element[0].attrib[attribute] + '"'
 					else:
 						# Check for attribute value changes.
+						
 						for attribute in attributes:
+							# Some differences are OK.
+							if old_path == "/bill" and attribute == "updated": continue # don't care
+							if old_path.startswith("/bill/committees/committee[") and attribute == "code": continue # We didn't have subcommittee codes previously.
+							
+							# Ignore changes in case.
+							if old_element.attrib[attribute].lower() == new_element[0].attrib[attribute].lower(): continue
+						
 							attribute_diff = diff( old_element.attrib[attribute], new_element[0].attrib[attribute] )
 
 							if is_different( attribute_diff ):
-								# XXX: Note this currently ignores attributes that contain timestamp data.
-								if attribute not in [ "updated", "datetime" ]:# or parameter:
-									print "* Attribute has difference in values:", old_path + "@" + attribute
-									print "** Old:", '"' + old_element.attrib[attribute] + '"'
-									print "** New:", '"' + new_element[0].attrib[attribute] + '"'
-									print "** Diff:", attribute_diff
+								print bill_id, "* Attribute has difference in values:", old_path + "@" + attribute
+								print bill_id, "** Old:", '"' + old_element.attrib[attribute] + '"'
+								print bill_id, "** New:", '"' + new_element[0].attrib[attribute] + '"'
+								#print bill_id, "** Diff:", attribute_diff
 
 				if ( old_element.text is not None ) and ( new_element[0].text is not None ):
 					old_element_text = old_element.text.strip()
 					new_element_text = new_element[0].text.strip()
 
+					# Some differences are OK.
+					if old_path.startswith("/bill/actions/action[") and old_path.endswith("/text"): continue # don't care
+					if old_path == "/bill/summary" and len(old_element_text) > 0 and len(new_element_text) > 0: continue # don't care
+					
 					# XXX: simplediff takes forever on long spans of text; splitting on newline speeds things up, but sacrifices granularity.
 					if old_element.tag == "summary":
 						text_diff = diff( old_element_text.split( "\n" ), new_element_text.split( "\n" ) )
@@ -139,17 +134,17 @@ for old, new in [ ( "h", "hr" ), ( "hr", "hres" ), ( "hj", "hjres" ), ( "hc", "h
 						text_diff = diff( old_element_text, new_element_text )
 
 					if is_different( text_diff ):
-						print "* Element text differs:", old_path
+						print bill_id, "* Element text differs:", old_path
 
 						# XXX: Don't output text contents of really long elements.
 						if old_element.tag in [ "text", "summary" ]:
-							print "** (Text contents omitted for brevity.)"
+							print bill_id, "** (Text contents omitted for brevity.)"
 						else:
-							print "** Old:", '"' + old_element_text + '"'
-							print "** New:", '"' + new_element_text + '"'
-							print "** Diff:", text_diff
+							print bill_id, "** Old:", '"' + old_element_text + '"'
+							print bill_id, "** New:", '"' + new_element_text + '"'
+							#print bill_id, "** Diff:", text_diff
 
-		print "Done checking %s%d.\n" % ( new, bill )
+		#print "Done checking %s%d.\n" % ( new, bill )
 
 		bill += 1
 
